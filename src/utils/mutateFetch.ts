6e6e810297968db5b0f2cc94e86fea4043b39c9b
Kusuma Sandi <71799886+miracle8oys@users.noreply.github.com>
2023-03-15 09:51:23 +0800

    Update mutateFetch.ts

--- src/utils/mutateFetch.ts
+++ src/utils/mutateFetch.ts
@@ -3,29 +3,23 @@ const base_url = import.meta.env.VITE_BASE_URL;
 const mutateFetch = async (
   url: string,
   body: Object,
-  method: "POST" | "PUT" | "DELETE",
-  refreshToken?: string
+  method: "POST" | "PUT" | "DELETE"
 ) => {
   const access_token = localStorage.getItem("accessToken");
-  const tokenExpiredCode = 406;
+  const tokenExpiredCode = 403;
 
   return await fetch(`${base_url + url}`, {
     method: method,
     headers: {
       "Content-Type": "application/json",
-      "x-access-token": refreshToken || access_token!,
+      "x-access-token": access_token!,
     },
     body: JSON.stringify({ ...body }),
   }).then((res) => {
     // If token expired, hit refresh token api and rerun the function
     if (res.status === tokenExpiredCode) {
       const refresh_token = localStorage.getItem("refreshToken");
-      mutateFetch(
-        "refresh-token-url",
-        { userCredential: "unknown" },
-        "POST",
-        refresh_token!
-      )
+      mutateFetch("/refresh", { refreshToken: refresh_token }, "POST")
         .then((res) => {
           if (!res.ok) {
             console.log("Token Expired");
@@ -34,11 +28,10 @@ const mutateFetch = async (
           return res.json();
         })
         .then((result) => {
-          localStorage.setItem("accessToken", result.accessToken);
-          localStorage.setItem("refreshToken", result.refreshToken);
+          localStorage.setItem("accessToken", result.data.accessToken);
           mutateFetch(url, body, "POST");
         });
-//       return (window.location.href = "/login");
+      //       return (window.location.href = "/login");
     }
     if (!res.ok) {
       throw new Error("Request Failed");
